0204cdddb1077b828f01ec86eb325e55a17cb32f
Renato <re.rosa98@gmail.com>
2023-12-20 20:51:52 -0300

    setup supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "shadcn-nuxt",
     ["@nuxtjs/google-fonts", { families: { Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900] } }],
+    "@nuxtjs/supabase",
   ],
   shadcn: {
     /**
@@ -22,5 +23,12 @@ export default defineNuxtConfig({
   },
   googleFonts: {
     preload: true,
+  },
+  supabase: {
+    redirectOptions: {
+      login: "/login",
+      callback: "confirm",
+      exclude: ["/"],
+    },
   },
 });
